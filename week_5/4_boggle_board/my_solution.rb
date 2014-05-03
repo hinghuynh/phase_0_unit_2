27c14fa43aa744d822307d504eb5f1264dd577a0
hinghuynh <hinghuynh@gmail.com>
2014-05-03 08:35:39 -0700

    Update my_solution.rb

--- week_5/4_boggle_board/my_solution.rb
+++ week_5/4_boggle_board/my_solution.rb
@@ -28,6 +28,9 @@ puts create_word(boggle_board, [2,1], [1,1], [1,2], [0,3])  #=> returns "code"
 puts create_word(boggle_board, [0,1], [0,2], [1,2])  #=> creates what california slang word?
 
 # Reflection 
+# This exercise was done already for us, so all we really had to do was understand what
+# the code was doing. It didn't take too long to understand the code after playing running
+# the driver tests.  This solution gave me a better understanding of passing multiple arguments.
 
 #-------------------------------------------------------------------------------
 
@@ -54,8 +57,9 @@ end
 get_row(1) #=>  ["i", "o", "d", "t"]
 
 # Reflection 
-
-
+# This was a fairly simple exercise, after we figured out that we could turn
+# boggle_board array into a global variable in order for the method to get access
+# to the array. Getting the row out of the boggle board was fairly straight-forward.
 
 #-------------------------------------------------------------------------------
 
@@ -71,11 +75,14 @@ def get_col(col)
     $boggle_board.transpose[col]
 end
 
-
 # Refactored Solution
-
+# Nothing here.
 
 # DRIVER TESTS GO BELOW THIS LINE
 get_col(1)  #=>  ["r", "o", "c", "a"]
 
 # Reflection 
+# The difficultly for this exercise was figuring out if there was a method we could use to grab the
+# column out of the array or if we have to run a loop to grab each element individually. I was able
+# to stumble upon the method transpose, which worked beautifully. I get the feeling that the exercise,
+# wasn't intended to be as easy as I made it. I'm very curious as to how others approached these exercises.
