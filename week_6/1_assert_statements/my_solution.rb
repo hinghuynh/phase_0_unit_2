d38e5508d2209fa520116da101f6baf25f5128e4
hinghuynh <hinghuynh@gmail.com>
2014-05-03 12:26:57 -0700

    Update my_solution.rb

--- week_6/1_assert_statements/my_solution.rb
+++ week_6/1_assert_statements/my_solution.rb
@@ -1,17 +1,17 @@
 # U2.W6: Testing Assert Statements
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself, with: Lienha Carleton].
 
 
 # 2. Review the simple assert statement
 
-def assert
-  raise "Assertion failed!" unless yield
-end
+# def assert
+#   raise "Assertion failed!" unless yield
+# end
 
-name = "bettysue"
-assert { name == "bettysue" }
-assert { name == "billybob" }
+# name = "bettysue"
+# assert { name == "bettysue" }
+# assert { name == "billybob" }
 
 # 2. Pseudocode what happens when the code above runs
 # create method called assert to raise argument error assertion failed unless yield
@@ -22,14 +22,33 @@ assert { name == "billybob" }
 # because statement is false.
 
 # 3. Copy your selected challenge here
-
+class CreditCard
+  def initialize(card)
+    raise ArgumentError.new("Invalid number of digits") unless card.to_s.length == 16
+    @card = card.to_s.chars.map(&:to_i)  
+  end
+  def check_card
+    i = @card.length - 2
+    while i >= 0
+      @card[i] *= 2
+      i -= 2
+    end
+    @card = @card.to_s.chars.map(&:to_i)
+    @card.reduce(:+) % 10 == 0
+  end
+end
+card = CreditCard.new(4408041234567893)
+p card.check_card == true
+another_card = CreditCard.new(4408041234567892)
+p another_card.check_card == false
 
 
 
 # 4. Convert your driver test code from that challenge into Assert Statements
-
-
-
-
+def assert
+  raise "Assertion failed!" unless yield
+end
+assert { card.check_card == true }
+assert { another_card.check_card == true }
 
 # 5. Reflection
