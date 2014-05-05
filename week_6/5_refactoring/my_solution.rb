0c36ca134af010ddeea17fa9ca71f0a1d004c045
hinghuynh <hinghuynh@gmail.com>
2014-05-05 07:52:26 -0700

    Update my_solution.rb

--- week_6/5_refactoring/my_solution.rb
+++ week_6/5_refactoring/my_solution.rb
@@ -6,18 +6,18 @@
 
 # Original Solution
 
-class CreditCard
-	def initialize(num)
-		raise(ArgumentError, 'invald # of digits') unless num.to_s.length == 16
-		@num = num.to_s.chars.map(&:to_i)
-	end
+# class CreditCard
+# 	def initialize(num)
+# 		raise(ArgumentError, 'invald # of digits') unless num.to_s.length == 16
+# 		@num = num.to_s.chars.map(&:to_i)
+# 	end
 
-	def check_card
-		@num.each_index { |i| @num[i] *= 2 if i.even? }
-		@num = @num.join.to_s.chars.map(&:to_i).
-		@num.reduce(:+) % 10 == 0
-	end
-end
+# 	def check_card
+# 		@num.each_index { |i| @num[i] *= 2 if i.even? }
+# 		@num = @num.join.to_s.chars.map(&:to_i).
+# 		@num.reduce(:+) % 10 == 0
+# 	end
+# end
 
 # Refactored Solution
 
