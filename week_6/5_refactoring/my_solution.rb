091a3e807ed8d5c16e24c4fd53c39d3375c348d7
hinghuynh <hinghuynh@gmail.com>
2014-05-05 07:47:31 -0700

    Update my_solution.rb

--- week_6/5_refactoring/my_solution.rb
+++ week_6/5_refactoring/my_solution.rb
@@ -5,16 +5,35 @@
 
 
 # Original Solution
+# 4. Refactored Solution
+class CreditCard
+	def initialize(num)
+		raise(ArgumentError, 'invald # of digits') unless num.to_s.length == 16
+		@num = num.to_s.chars.map(&:to_i)
+	end
 
+	def check_card
+		@num.each_index { |i| @num[i] *= 2 if i.even? }
+		@num = @num.join.to_s.chars.map(&:to_i).
+		@num.reduce(:+) % 10 == 0
+	end
+end
 
 
 
 # Refactored Solution
 
+# 4. Refactored Solution
+class CreditCard
+	def initialize(num)
+		raise(ArgumentError, 'invald # of digits') unless num.to_s.length == 16
+		@num = num.to_s.chars.map(&:to_i)
+	end
 
-
-
-
+	def check_card
+		@num.each_index { |i| @num[i] *= 2 if i.even? }.join.to_s.chars.map(&:to_i).reduce(:+) % 10 == 0
+	end
+end
 
 # DRIVER TESTS GO BELOW THIS LINE
 
